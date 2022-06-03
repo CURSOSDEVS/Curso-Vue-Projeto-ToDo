75d47a04383ee210d20ecd75f3cfe0dc0dde4e06
CURSOSDEVS <claudisneibello@hotmail.com>
2022-06-03 18:10:03 -0300

    Iniciando o projeto

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+		<TaskList :tasks="tasks"/>
+	</div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+	components:{ TaskList},
+	data() {
+		return {
+			tasks:[
+				{name: 'Comprar Blusa', pending: true},
+				{name: 'Ler um livro', pending: false}
+			]
+		}
+	},
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="task-list">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+        </template>
+        <p v-else>Sua vida está em dia :)</p>
+    </div>    
+</template>
+
+<script>
+    import Task from './Task.vue'
+
+    export default{
+        components:{ Task},
+        props:{
+            tasks:{ type: Array, required: true}
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
+
